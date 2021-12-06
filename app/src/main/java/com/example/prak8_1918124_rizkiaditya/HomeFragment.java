53a3cc41701359660879fb0fc96303ff75c4c87c
rizkiaditya7 <rizkiaditya20@gmail.com>
2021-12-06 17:28:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/prak8_1918124_rizkiaditya/HomeFragment.java
@@ -0,0 +1,71 @@
+package com.example.prak8_1918124_rizkiaditya;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class HomeFragment extends Fragment {
+
+    String api_key = "8402c0cece6a448aa1543b47c18369da";
+    ArrayList<ModelClass> modelClassArrayList;
+    Adapter adapter;
+    String country="id";
+    private RecyclerView recyclerViewhome;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle
+                                     savedInstanceState) {
+        View v = inflater.inflate(R.layout.homefragment,null);
+
+        recyclerViewhome=v.findViewById(R.id.recyclerhome);
+        modelClassArrayList = new ArrayList<>();
+        recyclerViewhome.setLayoutManager(new
+                LinearLayoutManager(getContext()));
+        adapter = new
+                Adapter(getContext(),modelClassArrayList);
+        recyclerViewhome.setAdapter(adapter);
+
+        findNews();
+
+        return v;
+    }
+
+    private void findNews() {
+
+
+        ApiUtilities.getApiInterface().getNews(country,100,api_key).enqueue(new Callback<mainNews>() {
+            @Override
+            public void onResponse(Call<mainNews> call,
+                                   Response<mainNews> response) {
+                if(response.isSuccessful())
+                {
+
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<mainNews> call,
+                                  Throwable t) {
+
+            }
+        });
+
+    }
+}
